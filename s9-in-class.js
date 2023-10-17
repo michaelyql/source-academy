2d9375ee575b51855d0ca283f2e96ed3faed2852
Michael Yang <No public email provided>
2023-10-17 11:40:39 +0800

    Changes made from Source Academy

---
+++ s9-in-class.js
@@ -0,0 +1,53 @@
+function append_to_array(x, arr) {
+    const l = array_length(arr);
+    arr[l] = x; 
+}
+
+// Array version
+function count_pairs(x, counted) {
+    let is_counted = false;
+    for (let i = 0; i < array_length(counted); i = i + 1) {
+        if (x === counted[i]) {
+            is_counted = true;
+        }
+    }
+    if (!is_counted) {
+        if (!is_pair(x)) {
+            return 0;
+        } else {
+            append_to_array(x, counted); 
+            return 1 + count_pairs(head(x), counted) + count_pairs(tail(x), counted);
+        }
+    } else {
+        return 0;
+    }
+}
+
+const y = pair(4, null);
+const double_counted = pair(pair(3,y), pair(7, y));
+
+const cycle = list(1,2,3);
+set_tail(tail(tail(cycle)), cycle); 
+
+count_pairs(cycle, []); 
+
+// List version
+
+function count_pairs_using_list(x) {
+    let lst = list();
+    function helper(x) {
+        if (is_null(member(x, lst))) {
+            if (!is_pair(x)) {
+                return 0;
+            } else {
+                lst = pair(x, lst); 
+                return 1 + helper(head(x)) + helper(tail(x)); 
+            }
+        } else {
+            return 0;
+        }
+    }
+    return helper(x);
+}
+
+count_pairs_using_list(double_counted);
